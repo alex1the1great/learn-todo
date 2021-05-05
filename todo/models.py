ea2ece5959771edf16a468783d0b9103f914a800
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-05 12:10:39 +0545

    refactor: change default value to false

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     name = models.CharField(max_length=200)
-    is_completed = models.BooleanField(default=True)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
