5de533611248efc7b211b79f9c55522be20181bc
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-05 11:02:21 +0545

    fix: change datetime field to boolean field

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     name = models.CharField(max_length=200)
-    is_completed = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
